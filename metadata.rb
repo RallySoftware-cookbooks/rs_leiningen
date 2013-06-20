45043e522637c2c07fbb3232417e65744ff4085a
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-06-19 22:48:01 -0600

    converted from double to single quotes, fixed maintainer name/email address/license

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "leiningen"
-maintainer       "The Fellowship"
-maintainer_email "thefellowship@rallydev.com"
-license          "All rights reserved"
-description      "Installs/Configures leiningen"
+name             'leiningen'
+maintainer       'Rally Software Development Corp'
+maintainer_email 'rallysoftware-cookbooks@rallydev.com'
+license          'MIT'
+description      'Installs/Configures leiningen'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
 recipe 'leiningen::default', 'Installs leiningen'
 
