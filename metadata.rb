822ab7d791fa55b14fadef733eb8b1d18f7ecf6a
Russ Teabeault and Matt Farrar <rteabeault@rallydev.com>
2013-10-10 11:11:00 -0600

    Updating to use VERSION file.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rallysoftware-cookbooks@rallydev.com'
 license          'MIT'
 description      'Installs/Configures leiningen'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 recipe 'leiningen::default', 'Installs leiningen'
 
