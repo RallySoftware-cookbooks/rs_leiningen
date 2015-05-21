ea35f1aa4936f09bd2b4f942d50a12bfe2f6c514
Jeff Smith <jsmith@rallydev.com>
2015-05-21 11:37:45 -0600

    fuck you single quoted strings food critic rule

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:leiningen][:version] = '2.5.1'
-default[:leiningen][:install_script_prefix] = "https://raw.github.com/technomancy/leiningen/"
-default[:leiningen][:install_script_suffix] = "/bin/lein"
+default[:leiningen][:install_script_prefix] = 'https://raw.github.com/technomancy/leiningen/'
+default[:leiningen][:install_script_suffix] = '/bin/lein'
 default[:leiningen][:checksum] = 'af77a8569238fb89272fdd46974c97383be126f19e709f1e7b1c5ffb9135e1d7'
 default[:leiningen][:user] = 'root'
 default[:leiningen][:group] = 'root'
